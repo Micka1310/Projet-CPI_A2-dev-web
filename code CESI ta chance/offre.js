75c8d9d4a592d3da598cbd7c17f815d840be9202
mickaël King <mickaelking13@gmail.com>
2025-02-25 19:12:55 +0100

    ajout d'animation pour l'en-tête

--- code CESI ta chance/offre.js
+++ code CESI ta chance/offre.js
@@ -1,23 +1,54 @@
-/*pour faire apparaître/disparaitre les liens secondaires du bouton burger 
-en appuyant sur le bouton burger (dekstop)*/
 const bouton_burger_1 = document.getElementById("bouton_burger_1");
+const bouton_burger_2 = document.getElementById("bouton_burger_2");
 const lien_burger_2 = document.getElementById("lien_burger_2");
+const lien_profil = document.getElementById("lien_profil_1");
+const profil = document.getElementById("profil");
+const bouton_x_1 = document.getElementById("x_1");
+const bouton_x_2 = document.getElementById("x_2");
+
+let profil_afficher = false;
 let lien_afficher = false;
 
+/*pour faire apparaître/disparaitre les liens secondaires du bouton burger 
+en appuyant sur le bouton burger (dekstop)*/
 function clique_burger_1()
 {
     if (lien_afficher == false)
     {
-        lien_burger_2.style.display = "flex";
-        lien_afficher = true;
-        console.log("fonction clique_burger_1 : boucle \'==false\'");
+        if (window.innerWidth > 899)
+        {
+            lien_burger_2.style.display = "flex";
+            lien_burger_2.style.width = "auto"
+            lien_profil.style.display = "none";
+            lien_afficher = true;
+            profil_afficher = false;
+            console.log("fonction clique_burger_1 : boucle if ('lien_afficher == false) -> boucle (window.innerWidth > 899)");
+        }
+
+        else
+        {
+            lien_burger_2.style.transform = "translateX(0%)";
+            /*bouton_x_2.style.display = "flex";*/
+            lien_afficher = true;
+            console.log("fonction clique_burger_1 : boucle if (lien_afficher == false) -> boucle else");
+        }
     }
 
     else
     {
-        lien_burger_2.style.display = "none";
-        lien_afficher = false;
-        console.log("fonction clique_burger_1 : boucle \'else\'");
+        if (window.innerWidth > 899)
+        {
+            lien_burger_2.style.display = "none";
+            lien_afficher = false;
+            console.log("fonction clique_burger_1 : boucle else -> boucle if (window.innerWidth > 899)");
+        }
+
+        /*else
+        {
+            lien_burger_2.style.right = "-85%";
+            lien_afficher = false;
+            console.log("fonction clique_burger_1 : boucle else -> boucle else");
+        }*/
     }
 }
 
@@ -26,18 +57,15 @@ bouton_burger_1.addEventListener("click", clique_burger_1);
 
 /*pour faire apparaître/disparaitre les liens secondaires du bouton burger 
 en appuyant sur le bouton burger (mobile)*/
-const bouton_burger_2 = document.getElementById("bouton_burger_2");
-
 bouton_burger_2.addEventListener("click", clique_burger_1);
 
 
 /*pour faire disparaitre les liens secondaires du bouton burger 
 en appuyant sur la croix*/
-const bouton_x_2 = document.getElementById("x_2");
-
 function clique_bouton_x_2()
 {
-    lien_burger_2.style.display = "none";
+    /*bouton_x_2.style.display = "none";*/
+    lien_burger_2.style.transform = "translateX(100%)";
     lien_afficher = false;
     console.log("fonction clique_bouton_x_2");
 }
@@ -47,25 +75,36 @@ bouton_x_2.addEventListener("click", clique_bouton_x_2);
 
 /*pour faire apparaître/disparaitre le nom/prenom de l'utilisateur et l'option 
 de deconnexion en appuyant sur le profil (dekstop)*/
-const profil = document.getElementById("profil");
-const lien_profil = document.getElementById("lien_profil_1");
-
-let profil_afficher = false;
-
 function clique_profil()
 {
     if (profil_afficher == false)
     {
-        lien_profil.style.display = "flex";
-        profil_afficher = true;
-        console.log("fonction clique_profil : \'==false\'");
+        if (window.innerWidth > 899)
+        {
+            lien_profil.style.display = "flex";
+            lien_burger_2.style.display = "none";
+            profil_afficher = true;
+            lien_afficher = false;
+            console.log("fonction clique_profil : boucle if (profil_afficher == false) -> if (window.innerWidth > 899)");
+        }
+
+        else
+        {
+            lien_profil.style.transform = "translateX(0%)";
+            /*bouton_x_2.style.display = "flex";*/
+            profil_afficher = true;
+            console.log("fonction clique_profil : boucle if (lien_afficher == false) -> boucle else");
+        }
     }
 
     else
     {
-        lien_profil.style.display = "none";
-        profil_afficher = false;
-        console.log("fonction clique_profil : \'else\'");
+        if (window.innerWidth > 899)
+        {
+            lien_profil.style.display = "none";
+            profil_afficher = false;
+            console.log("fonction clique_profil : else");
+        }
     }
 }
 
@@ -74,13 +113,46 @@ profil.addEventListener("click", clique_profil);
 
 /*pour faire disparaitre le nom/prenom de l'utilisateur et l'option de deconnexion 
 en appuyant sur la croix*/
-const bouton_x_1 = document.getElementById("x_1");
-
 function clique_bouton_x_1()
 {
-    lien_profil.style.display = "none";
+    lien_profil.style.transform = "translateX(100%)";
     profil_afficher = false;
     console.log("fonction clique_bouton_x_1");
 }
 
 bouton_x_1.addEventListener("click", clique_bouton_x_1);
+
+
+/*pour faire disparaître les affichages du bouton profil/burger
+lorsque l'on clique autre part dans la page que ces affichages*/
+function clique_page_1 (cible)
+{
+    if 
+    (
+        lien_afficher == true 
+        && !lien_burger_2.contains(cible.target) 
+        && !bouton_burger_1.contains(cible.target) 
+        && !bouton_burger_2.contains(cible.target)
+        && window.innerWidth > 899
+    ) 
+    {
+        lien_burger_2.style.display = "none";
+        lien_afficher = false;
+        console.log("clique_page_1 : if (lien_afficher == true)");
+    }
+
+    if 
+    (
+        profil_afficher == true 
+        && !lien_profil.contains(cible.target) 
+        && !profil.contains(cible.target)
+        && window.innerWidth > 899
+    ) 
+    {
+        lien_profil.style.display = "none";
+        profil_afficher = false;
+        console.log("clique_page_1 : if (profil_afficher == true)");
+    }
+}
+
+document.addEventListener("click", clique_page_1);
