8224e5ddd50f2f96191cddde3f8c174bb37ee24d
mickaël King <mickaelking13@gmail.com>
2025-02-27 22:44:09 +0100

    Amélioration de l'en-tête en supprimant le second bouton burger

--- code CESI ta chance/en-tete/en-tete.js
+++ code CESI ta chance/en-tete/en-tete.js
@@ -4,7 +4,8 @@ const bouton_burger_1 = document.getElementById("bouton_burger_1");
 const bouton_burger_2 = document.getElementById("bouton_burger_2");
 const lien_burger_2 = document.getElementById("lien_burger_2");
 const lien_profil = document.getElementById("lien_profil_1");
-const profil = document.getElementById("profil");
+const profil_1 = document.getElementById("profil_1");
+const profil_2 = document.getElementById("profil_2");
 const bouton_x_1 = document.getElementById("x_1");
 const bouton_x_2 = document.getElementById("x_2");
 
@@ -59,7 +60,6 @@ bouton_burger_2.addEventListener("click", clique_burger_1);
 en appuyant sur la croix*/
 function clique_bouton_x_2()
 {
-    /*bouton_x_2.style.display = "none";*/
     lien_burger_2.style.transform = "translateX(100%)";
     lien_afficher = false;
     console.log("fonction clique_bouton_x_2");
@@ -86,7 +86,6 @@ function clique_profil()
         else
         {
             lien_profil.style.transform = "translateX(0%)";
-            /*bouton_x_2.style.display = "flex";*/
             profil_afficher = true;
             console.log("fonction clique_profil : boucle if (lien_afficher == false) -> boucle else");
         }
@@ -103,11 +102,16 @@ function clique_profil()
     }
 }
 
-profil.addEventListener("click", clique_profil);
+profil_1.addEventListener("click", clique_profil);
+
+
+/*pour faire apparaître/disparaitre le nom/prenom de l'utilisateur et l'option 
+de deconnexion en appuyant sur le profil (mobile)*/
+profil_2.addEventListener("click", clique_profil);
 
 
 /*pour faire disparaitre le nom/prenom de l'utilisateur et l'option de deconnexion 
-en appuyant sur la croix*/
+en appuyant sur la croix (mobile)*/
 function clique_bouton_x_1()
 {
     lien_profil.style.transform = "translateX(100%)";
@@ -119,7 +123,7 @@ bouton_x_1.addEventListener("click", clique_bouton_x_1);
 
 
 /*pour faire disparaître les affichages du bouton profil/burger
-lorsque l'on clique autre part dans la page que ces affichages*/
+lorsque l'on clique autre part dans la page que ces affichages (dekstop)*/
 function clique_page_1 (cible)
 {
     if 
@@ -140,7 +144,7 @@ function clique_page_1 (cible)
     (
         profil_afficher == true 
         && !lien_profil.contains(cible.target) 
-        && !profil.contains(cible.target)
+        && !profil_1.contains(cible.target)
         && window.innerWidth > 899
     ) 
     {
--- code CESI ta chance/offre.js
+++ code CESI ta chance/offre.js
@@ -4,7 +4,8 @@ const bouton_burger_1 = document.getElementById("bouton_burger_1");
 const bouton_burger_2 = document.getElementById("bouton_burger_2");
 const lien_burger_2 = document.getElementById("lien_burger_2");
 const lien_profil = document.getElementById("lien_profil_1");
-const profil = document.getElementById("profil");
+const profil_1 = document.getElementById("profil_1");
+const profil_2 = document.getElementById("profil_2");
 const bouton_x_1 = document.getElementById("x_1");
 const bouton_x_2 = document.getElementById("x_2");
 
@@ -59,7 +60,6 @@ bouton_burger_2.addEventListener("click", clique_burger_1);
 en appuyant sur la croix*/
 function clique_bouton_x_2()
 {
-    /*bouton_x_2.style.display = "none";*/
     lien_burger_2.style.transform = "translateX(100%)";
     lien_afficher = false;
     console.log("fonction clique_bouton_x_2");
@@ -86,7 +86,6 @@ function clique_profil()
         else
         {
             lien_profil.style.transform = "translateX(0%)";
-            /*bouton_x_2.style.display = "flex";*/
             profil_afficher = true;
             console.log("fonction clique_profil : boucle if (lien_afficher == false) -> boucle else");
         }
@@ -103,11 +102,16 @@ function clique_profil()
     }
 }
 
-profil.addEventListener("click", clique_profil);
+profil_1.addEventListener("click", clique_profil);
+
+
+/*pour faire apparaître/disparaitre le nom/prenom de l'utilisateur et l'option 
+de deconnexion en appuyant sur le profil (mobile)*/
+profil_2.addEventListener("click", clique_profil);
 
 
 /*pour faire disparaitre le nom/prenom de l'utilisateur et l'option de deconnexion 
-en appuyant sur la croix*/
+en appuyant sur la croix (mobile)*/
 function clique_bouton_x_1()
 {
     lien_profil.style.transform = "translateX(100%)";
@@ -119,7 +123,7 @@ bouton_x_1.addEventListener("click", clique_bouton_x_1);
 
 
 /*pour faire disparaître les affichages du bouton profil/burger
-lorsque l'on clique autre part dans la page que ces affichages*/
+lorsque l'on clique autre part dans la page que ces affichages (dekstop)*/
 function clique_page_1 (cible)
 {
     if 
@@ -140,7 +144,7 @@ function clique_page_1 (cible)
     (
         profil_afficher == true 
         && !lien_profil.contains(cible.target) 
-        && !profil.contains(cible.target)
+        && !profil_1.contains(cible.target)
         && window.innerWidth > 899
     ) 
     {
@@ -151,3 +155,32 @@ function clique_page_1 (cible)
 }
 
 document.addEventListener("click", clique_page_1);
+
+
+/*pour changer d'image d'ampoule dans la barre de recherche 
+lorsque la souris et sur l'image*/
+const ampoule_eteinte = document.getElementById('ampoule_eteinte');
+const ampoule_allumer = document.getElementById('ampoule_allumer');
+
+function allume_ampoule ()
+{
+    ampoule_eteinte.style.visibility = "hidden";
+    ampoule_eteinte.style.opacity = "0";
+    ampoule_allumer.style.visibility = "visible";
+    ampoule_allumer.style.opacity = "1";
+    //pour debugger
+    /*console.log("allume_ampoule");*/
+}
+
+function eteint_ampoule ()
+{
+    ampoule_eteinte.style.visibility = "visible";
+    ampoule_eteinte.style.opacity = "1";
+    ampoule_allumer.style.visibility = "hidden";
+    ampoule_allumer.style.opacity = "0";
+    //pour debugger
+    /*console.log("eteint_ampoule");*/
+}
+
+ampoule_eteinte.addEventListener("mouseover", allume_ampoule);
+ampoule_allumer.addEventListener("mouseout", eteint_ampoule);
