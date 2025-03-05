4be8e57c171241ec8e509fa6e038f58829fa9fd1
Micka13 <150048395+Micka1310@users.noreply.github.com>
2025-03-05 11:19:48 +0100

    Mise à jour de l'en-tête v2 (#12)

--- code CESI ta chance/offre.js
+++ code CESI ta chance/offre.js
@@ -44,6 +44,7 @@ function clique_burger_1()
             lien_profil_1.style.display = "none";
             lien_afficher = true;
             profil_afficher = false;
+            //pour debugger
             console.log("fonction clique_burger_1 : !lien_afficher -> verif_mode_paysage");
         }
 
@@ -199,6 +200,84 @@ function clique_page_1 (cible)
 document.addEventListener("click", clique_page_1);
 
 
+/*pour changer d'image d'ampoule dans la barre de recherche 
+lorsque la souris et sur l'image (paysage et portrait)*/
+const ampoule_eteinte_1 = document.getElementById('ampoule_eteinte_1');
+const ampoule_allumer_1 = document.getElementById('ampoule_allumer_1');
+const barre_recherche_offre_1 = document.getElementById("barre_recherche_offre_1");
+const barre_recherche_offre_2 = document.getElementById("barre_recherche_offre_2");
+const ampoule_eteinte_2 = document.getElementById('ampoule_eteinte_2');
+const ampoule_allumer_2 = document.getElementById('ampoule_allumer_2');
+
+function allume_ampoule ()
+{
+    if (barre_recherche_offre_1.value.length > 0 || barre_recherche_offre_2.value.length > 0)
+    {
+        ampoule_eteinte_1.style.visibility = "hidden";
+        ampoule_eteinte_1.style.opacity = "0";
+        ampoule_allumer_1.style.visibility = "visible";
+        ampoule_allumer_1.style.opacity = "1";
+        ampoule_eteinte_2.style.visibility = "hidden";
+        ampoule_eteinte_2.style.opacity = "0";
+        ampoule_allumer_2.style.visibility = "visible";
+        ampoule_allumer_2.style.opacity = "1";
+        //pour debugger
+        //console.log("allume_ampoule : if barre_recherche_offre_1");
+    }
+
+    else
+    {
+        ampoule_eteinte_1.style.visibility = "visible";
+        ampoule_eteinte_1.style.opacity = "1";
+        ampoule_allumer_1.style.visibility = "hidden";
+        ampoule_allumer_1.style.opacity = "0";
+        ampoule_eteinte_2.style.visibility = "visible";
+        ampoule_eteinte_2.style.opacity = "1";
+        ampoule_allumer_2.style.visibility = "hidden";
+        ampoule_allumer_2.style.opacity = "0";
+        //pour debugger
+        //console.log("allume_ampoule : else");
+    }
+}
+
+barre_recherche_offre_1.addEventListener("input", allume_ampoule);
+barre_recherche_offre_2.addEventListener("input", allume_ampoule);
+
+
+/*pour afficher/cacher la barre de recherche de stage 
+en cliquant sur l'élément "recherche" ou ">" (portrait)*/
+const flex_container_recherche_1 = document.getElementById("flex_container_recherche_1");
+const flex_container_recherche_6 = document.getElementById("flex_container_recherche_6");
+const flex_container_recherche_7 = document.getElementById("flex_container_recherche_7");
+const bouton_mode_recherche_2 = document.getElementById("bouton_mode_recherche_2");
+
+let section_recherche_afficher = false;
+
+function affiche_recherhe()
+{
+    if (!section_recherche_afficher)
+    {
+        flex_container_recherche_1.style.transition = "transform 0.5s ease"
+        flex_container_recherche_1.style.transform = "translateX(0%)";
+        section_recherche_afficher = true;
+        //pour debugger
+        console.log("affiche_recherche : !section_recherche_afficher");
+    }
+
+    else
+    {
+        flex_container_recherche_1.style.transition = "transform 0.5s ease"
+        flex_container_recherche_1.style.transform = "translateX(-100%)";
+        section_recherche_afficher = false;
+        //pour debugger
+        console.log("affiche_recherche : else");
+    }
+}
+
+flex_container_recherche_7.addEventListener("click", affiche_recherhe);
+bouton_mode_recherche_2.addEventListener("click", affiche_recherhe);
+
+
 /*pour detecter l'orientation de l'affichage (paysage ou portrait)*/
 function changement_mode() 
 {
@@ -217,6 +296,22 @@ function changement_mode()
             console.log("changement_mode : verif_mode -> profil_afficher || lien_afficher");
         }
 
+        if (window.innerHeight < 600 && window.innerWidth < 940)
+        {
+            lien_profil_2.style.display = "flex";
+            lien_burger_1.style.display = "flex";
+        }
+
+        else 
+        {
+            lien_profil_2.style.display = "none";
+            lien_burger_1.style.display = "none";
+        }
+
+        lien_profil_1.style.boxShadow = "0vh 0vh 1.25vh #ff0000;";
+        lien_profil_2.style.boxShadow = "0vh 0vh 1.25vh #ff0000;";
+        lien_burger_1.style.boxShadow = "0vh 0vh 1.25vh #0099ff";
+        lien_burger_2.style.boxShadow = "0vh 0vh 1.25vh #0099ff";
         //pour debugger
         console.log("L'écran est maintenant en mode paysage");
     }
@@ -230,39 +325,15 @@ function changement_mode()
         lien_burger_2.style.display = "none";
         lien_burger_1.style.display = "flex";
         lien_afficher = false;
+        lien_profil_1.style.boxShadow = "0vh 0vh 0vh;";
+        lien_profil_2.style.boxShadow = "0vh 0vh 0vh";
+        lien_burger_1.style.boxShadow = "0vh 0vh 0vh";
+        lien_burger_2.style.boxShadow = "0vh 0vh 0vh";
         //pour debugger
         console.log("changement_mode : boucle else");
         console.log("L'écran est maintenant en mode portrait");
     }
 }
 
+changement_mode();
 mode_portrait.addEventListener("change", changement_mode);
-
-
-/*pour changer d'image d'ampoule dans la barre de recherche 
-lorsque la souris et sur l'image*/
-const ampoule_eteinte_1 = document.getElementById('ampoule_eteinte_1');
-const ampoule_allumer_1 = document.getElementById('ampoule_allumer_1');
-
-function allume_ampoule ()
-{
-    ampoule_eteinte_1.style.visibility = "hidden";
-    ampoule_eteinte_1.style.opacity = "0";
-    ampoule_allumer_1.style.visibility = "visible";
-    ampoule_allumer_1.style.opacity = "1";
-    //pour debugger
-    /*console.log("allume_ampoule");*/
-}
-
-function eteint_ampoule ()
-{
-    ampoule_eteinte_1.style.visibility = "visible";
-    ampoule_eteinte_1.style.opacity = "1";
-    ampoule_allumer_1.style.visibility = "hidden";
-    ampoule_allumer_1.style.opacity = "0";
-    //pour debugger
-    /*console.log("eteint_ampoule");*/
-}
-
-ampoule_eteinte_1.addEventListener("mouseover", allume_ampoule);
-ampoule_allumer_1.addEventListener("mouseout", eteint_ampoule);
